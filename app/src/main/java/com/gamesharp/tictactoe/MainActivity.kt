50df08ab52db5db9b0856d541da915b93afca8f6
Damir Asanbaev <damir9715@gmail.com>
2021-03-20 21:17:37 +0600

    feat: Add shapes

--- app/src/main/java/com/gamesharp/tictactoe/MainActivity.kt
+++ app/src/main/java/com/gamesharp/tictactoe/MainActivity.kt
@@ -3,23 +3,22 @@ package com.gamesharp.tictactoe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DimenRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.gamesharp.tictactoe.ui.theme.TicTacToeTheme
 
 private val LINE_WIDTH = 8.dp
+private val PLAYGROUND_SIZE = 324.dp
+private val CELL_SIZE = 108.dp
+private val FIGURE_SIZE = 80.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +26,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             TicTacToeTheme {
                 Surface(color = MaterialTheme.colors.surface) {
-                    Screen()
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(20.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Playground()
+                    }
                 }
             }
         }
@@ -35,60 +42,106 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Screen() {
-    Column(
-        Modifier
-            .fillMaxSize()
-            .padding(20.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Playground()
-    }
-}
-
-@Composable
 fun Playground() {
-    ConstraintLayout(Modifier.size(320.dp)) {
-        val (line1, line2, line3, line4) = createRefs()
+    Box {
+        Lines()
+        Shapes()
+    }
+}
 
+@Composable
+fun Shapes() {
+    Column {
+        Row(
+            Modifier
+                .height(CELL_SIZE)
+                .fillMaxWidth()
+        ) {
+            Shape(ShapeState.Cross)
+            Shape()
+            Shape()
+        }
+        Row(
+            Modifier
+                .height(CELL_SIZE)
+                .fillMaxWidth()) {
+            Shape()
+            Shape(ShapeState.Cross)
+            Shape()
+        }
+        Row(
+            Modifier
+                .height(CELL_SIZE)
+                .fillMaxWidth()
+        ) {
+            Shape(ShapeState.Empty)
+            Shape()
+            Shape(ShapeState.Cross)
+        }
+    }
+}
+
+@Composable
+fun Shape(state: ShapeState = ShapeState.Circle) {
+    Row(
+        modifier = Modifier
+            .size(CELL_SIZE)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        when (state) {
+            is ShapeState.Empty -> Unit
+            is ShapeState.Circle -> {
+                Image(
+                    modifier = Modifier.size(FIGURE_SIZE),
+                    painter = painterResource(R.drawable.ic_circle),
+                    contentDescription = null
+                )
+            }
+            is ShapeState.Cross -> {
+                Image(
+                    modifier = Modifier.size(FIGURE_SIZE),
+                    painter = painterResource(R.drawable.ic_cross),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Lines() {
+    Column(
+        modifier = Modifier.size(PLAYGROUND_SIZE),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
         Box(
             modifier = Modifier
-                .constrainAs(line1) {
-                    top.linkTo(parent.top)
-                    bottom.linkTo(line2.top)
-                }
                 .fillMaxWidth()
                 .height(LINE_WIDTH)
                 .background(MaterialTheme.colors.onSurface)
         )
         Box(
             modifier = Modifier
-                .constrainAs(line2) {
-                    top.linkTo(line1.bottom)
-                    bottom.linkTo(parent.bottom)
-                }
                 .fillMaxWidth()
                 .height(LINE_WIDTH)
                 .background(MaterialTheme.colors.onSurface)
         )
+    }
 
+    Row(
+        modifier = Modifier.size(PLAYGROUND_SIZE),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
         Box(
             modifier = Modifier
-                .constrainAs(line3) {
-                    start.linkTo(parent.start)
-                    end.linkTo(line4.start)
-                }
                 .width(LINE_WIDTH)
                 .fillMaxHeight()
                 .background(MaterialTheme.colors.onSurface)
         )
         Box(
             modifier = Modifier
-                .constrainAs(line4) {
-                    start.linkTo(line3.end)
-                    end.linkTo(parent.end)
-                }
                 .width(LINE_WIDTH)
                 .fillMaxHeight()
                 .background(MaterialTheme.colors.onSurface)
@@ -96,21 +149,8 @@ fun Playground() {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun Greeting() {
-    Column {
-        var name: String by remember { mutableStateOf("Hello") }
-
-        Text(
-            text = name,
-            modifier = Modifier.padding(bottom = 10.dp),
-            fontSize = 24.sp
-        )
-        TextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text(text = "Name") }
-        )
-    }
+sealed class ShapeState {
+    object Empty : ShapeState()
+    object Circle : ShapeState()
+    object Cross : ShapeState()
 }
